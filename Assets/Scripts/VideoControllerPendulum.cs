4a7ae86f825206d3707b87a1876d157ad097b3c4
Cesar Maiguel <cesar.maiguel@gmail.com>
2017-10-10 03:38:51 -0500

    Add pendulum script and video

--- Assets/Scripts/VideoControllerPendulum.cs
+++ Assets/Scripts/VideoControllerPendulum.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
 public class VideoControllerPendulum : MonoBehaviour {
+	float Timer = 57.0f;
+	ImageTargetPlayAudio imageTargetPlayAudio;
+
 
 	// Use this for initialization
 	void Start () {
-		
+		imageTargetPlayAudio = GetComponent<ImageTargetPlayAudio> (); 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
+
+		if (imageTargetPlayAudio.AudioFlag == 1) {
+			Timer -= Time.deltaTime;
+			if (Timer < 0) {
+				Handheld.PlayFullScreenMovie ("VideoPendulum.mp4", Color.black, FullScreenMovieControlMode.Hidden);
+			}
+
+		} else {
+			Timer = 57.0f;
+		}
+
 	}
 }
