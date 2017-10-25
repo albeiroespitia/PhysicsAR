49f4123a015745ddc990d7b49c4e54c355008728
Albeiro Espitia <albeiroespitiasierra@gmail.com>
2017-10-25 01:19:31 -0500

    Fix bugs

--- Assets/Scripts/ResetPosition.cs
+++ Assets/Scripts/ResetPosition.cs
@@ -8,27 +8,33 @@ using Vuforia;
 public class ResetPosition : MonoBehaviour {
 
 	Vector3 VernierinitialPosition;
+	Vector3 GeneratorinitialPosition;
 	Vector3 MicrometerinitialPosition;
 	Vector3 PenduluminitialPosition;
 	Vector3 VernierinitialScale;
+	Vector3 GeneratorinitialScale;
 	Vector3 MicrometerinitialScale; 
 	Vector3 PenduluminitialScale;
 	Quaternion MicrometerinitialRotation;
 	Quaternion VernierinitialRotation;
 	Quaternion PenduluminitialRotation;
+	Quaternion GeneratorinitialRotation;
 
 
 	// Use this for initialization
 	void Start () {
 		VernierinitialPosition = GameObject.Find ("VernierObj").GetComponent<InitalXYZ> ().initialPosition;
+		GeneratorinitialPosition = GameObject.Find ("GeneratorObj").GetComponent<InitalXYZ> ().initialPosition;
 		MicrometerinitialPosition = GameObject.Find ("MicrometerObj").GetComponent<InitalXYZ> ().initialPosition;
 		PenduluminitialPosition = GameObject.Find ("PendulumObj").GetComponent<InitalXYZ> ().initialPosition;	
 		VernierinitialScale = GameObject.Find ("VernierObj").GetComponent<InitalXYZ> ().initialScale;
+		GeneratorinitialScale = GameObject.Find ("GeneratorObj").GetComponent<InitalXYZ> ().initialScale;
 		MicrometerinitialScale = GameObject.Find ("MicrometerObj").GetComponent<InitalXYZ> ().initialScale;
 		PenduluminitialScale = GameObject.Find ("PendulumObj").GetComponent<InitalXYZ> ().initialScale;	
 		VernierinitialRotation = GameObject.Find ("VernierObj").GetComponent<InitalXYZ> ().initialRotation;
 		MicrometerinitialRotation = GameObject.Find ("MicrometerObj").GetComponent<InitalXYZ> ().initialRotation;
 		PenduluminitialRotation = GameObject.Find ("PendulumObj").GetComponent<InitalXYZ> ().initialRotation;
+		GeneratorinitialRotation = GameObject.Find ("GeneratorObj").GetComponent<InitalXYZ> ().initialRotation;
 
 		
 	}
@@ -48,6 +54,10 @@ public class ResetPosition : MonoBehaviour {
 			GameObject.Find ("PendulumObj").transform.position = PenduluminitialPosition;
 			GameObject.Find ("PendulumObj").transform.localScale = PenduluminitialScale;
 			GameObject.Find ("PendulumObj").transform.rotation = PenduluminitialRotation;
+		}else if(GameObject.Find("GeneratorQR").GetComponent<ImageTargetPlayAudio>().AudioFlag == 1){
+			GameObject.Find ("GeneratorObj").transform.position = GeneratorinitialPosition;
+			GameObject.Find ("GeneratorObj").transform.localScale = GeneratorinitialScale;
+			GameObject.Find ("GeneratorObj").transform.rotation = GeneratorinitialRotation;
 		}
 	}
 	
